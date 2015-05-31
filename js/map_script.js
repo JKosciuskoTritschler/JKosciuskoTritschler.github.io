e3645b2d031ece3dfdd365eca7a6ba2b768e11ae
JKosciuskoTritschler <jrtritschler@gmail.com>
2015-05-30 18:30:45 -0600

    Undefined Error

--- js/map_script.js
+++ js/map_script.js
@@ -3,8 +3,7 @@ function initialize() {
     center: { lat: -34.397, lng: 150.644},
     zoom: 8
   };
-  var map = new google.maps.Map(document.getElementById('.map-canvas'),
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
-};
-
-$(document).ready(initialize);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
