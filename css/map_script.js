6e6adbfeee1480cc1081635b80bcc0d2339afbed
JKosciuskoTritschler <jrtritschler@gmail.com>
2015-05-30 18:39:28 -0600

    Simplification of Scope

--- css/map_script.js
+++ css/map_script.js
@@ -3,7 +3,8 @@ function initialize() {
     center: { lat: -34.397, lng: 150.644},
     zoom: 8
   };
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
+  var map = new google.maps.Map(document.getElementById('.map-canvas'),
       mapOptions);
-}
-google.maps.event.addDomListener(window, 'load', initialize);
+};
+
+$(document).ready(initialize);
